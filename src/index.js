a9b59d2a1c10d5bb3d1e6d4b7576a80e159f7dcd
quannminh1603 <quannminh1603@gmail.com>
2024-01-08 18:54:09 +0700

    test run build

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
